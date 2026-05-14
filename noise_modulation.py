a38c6829b0fbfb3ded8a0d8d881c61d41913262f
sodacolour <48622577+sodacolor@users.noreply.github.com>
2026-05-14 04:57:07 +0100

    improved resolution scale parameter

--- noise_modulation.py
+++ noise_modulation.py
@@ -1,3 +1,4 @@
+import math
 import cv2 as cv
 import numpy as np
 import sys
@@ -8,15 +9,13 @@ import alive_progress
 
 class Modulator:
     def __init__(self, resolution: tuple[int, int]):
-        self._resolution = resolution
-
         # initialize buffer with random values between 0 and 1
         rng = np.random.default_rng()
-        self._buffer = rng.random(resolution)
+        self._buffer = rng.random(resolution[::-1])
 
     def modulate(self, source: np.ndarray, amount: float = 1):
         # process modulation source
-        source_resized = cv.resize(source, dsize = self._resolution, interpolation = cv.INTER_NEAREST) # resize modulation source to match internal buffer
+        source_resized = cv.resize(source, dsize = self._buffer.shape[2::-1], interpolation = cv.INTER_NEAREST) # resize modulation source to match internal buffer
         source_grayscale = cv.cvtColor(source_resized, cv.COLOR_RGB2GRAY)
         source_normalized = source_grayscale / 255
 
@@ -44,28 +43,26 @@ def render_image(args):
         raise RuntimeError("failed to load source image")
     source_resolution = (source.shape[0], source.shape[1])
 
-    source_file_name = os.path.basename(args.source)
-    if __name__ == "__main__":
-        print(f"loaded source image ({source_file_name})")
-
     # set unspecified arguments to default values
-    resolution = source_resolution if args.resolution is None else tuple(args.resolution)
     fps = 60 if args.fps is None else args.fps
     duration = 5 if args.duration is None else args.duration
     output_file_path = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S.mp4") if args.out is None else args.out
 
     # initialize video writer
-    output_resolution = resolution if args.output_unscaled else source_resolution
-    output = cv.VideoWriter(output_file_path, cv.VideoWriter.fourcc(*"mp4v"), fps, output_resolution)
-
-    modulator = Modulator(resolution)
+    buffer_resolution = tuple(map(lambda x: math.ceil(x * (args.resolution_scale / 100)), source_resolution))
+    modulator = Modulator(buffer_resolution)
     modulation_amount = args.rate / fps
 
+    # initialize video writer
+    output_resolution = buffer_resolution if args.output_unscaled else source_resolution
+    output = cv.VideoWriter(output_file_path, cv.VideoWriter.fourcc(*"mp4v"), fps, output_resolution)
+
+    source_file_name = os.path.basename(args.source)
     preview_window_title = f"render preview ({source_file_name})"
 
     frame_count = int(duration * fps)
     with alive_progress.alive_bar(frame_count, title = "rendering...") as progress_bar:
-        for i in range(frame_count):
+        for _ in range(frame_count):
             modulator.modulate(source, modulation_amount)
 
             match args.type:
@@ -77,7 +74,7 @@ def render_image(args):
                     raise ValueError(f"invalid 'type' argument supplied ('{args.type}')")
 
             # if necessary, resize frame to match output resolution
-            if not (frame.shape[0], frame.shape[1]) == output_resolution:
+            if frame.shape[:2] != output_resolution:
                 frame = cv.resize(frame, dsize = output_resolution, interpolation = cv.INTER_NEAREST)
 
             # if enabled, update render preview
@@ -104,27 +101,25 @@ def render_video(args):
     fps = int(source.get(cv.CAP_PROP_FPS))
     source_duration = int(source.get(cv.CAP_PROP_FRAME_COUNT)) / fps
 
-    source_file_name = os.path.basename(args.source)
-    if __name__ == "__main__":
-        print(f"loaded source video ({source_file_name})")
-
     # set unspecified arguments to default values
-    resolution = source_resolution if args.resolution is None else tuple(args.resolution)
     duration = source_duration if args.duration is None else args.duration
     output_file_path = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S.mp4") if args.out is None else args.out
 
-    # initialize video writer
-    output_resolution = resolution if args.output_unscaled else source_resolution
-    output = cv.VideoWriter(output_file_path, cv.VideoWriter.fourcc(*"mp4v"), fps, output_resolution)
-
-    modulator = Modulator(resolution)
+    # initialize modulator
+    buffer_resolution = tuple(map(lambda x: math.ceil(x * (args.resolution_scale / 100)), source_resolution))
+    modulator = Modulator(buffer_resolution)
     modulation_amount = args.rate / fps
 
+    # initialize video writer
+    output_resolution = buffer_resolution if args.output_unscaled else source_resolution
+    output = cv.VideoWriter(output_file_path, cv.VideoWriter.fourcc(*"mp4v"), fps, output_resolution)
+
+    source_file_name = os.path.basename(args.source)
     preview_window_title = f"render preview ({source_file_name})"
 
     frame_count = int(duration * fps)
     with alive_progress.alive_bar(frame_count, title = "rendering...") as progress_bar:
-        for i in range(frame_count):
+        for _ in range(frame_count):
             ret, frame = source.read()
             if not ret: # if the source has closed unexpectedly...
                 raise RuntimeError("source video closed unexpectedly")
@@ -140,7 +135,7 @@ def render_video(args):
                     raise ValueError(f"invalid 'type' argument supplied ('{args.type}')")
 
             # if necessary, resize frame to match output resolution
-            if not (frame.shape[0], frame.shape[1]) == output_resolution:
+            if frame.shape[:2] != output_resolution:
                 frame = cv.resize(frame, dsize = output_resolution, interpolation = cv.INTER_NEAREST)
 
             # if enabled, update render preview
@@ -168,7 +163,7 @@ if __name__ == "__main__":
     arg_parser.add_argument("source", help = "path to source image or video")
     arg_parser.add_argument("-t", "--type", choices = ["loop", "ping_pong"], default = "loop", help = "modulation type")
     arg_parser.add_argument("-r", "--rate", type = float, default = 10, help = "modulation rate (default is 10)")
-    arg_parser.add_argument("--resolution", nargs = 2, metavar = ("WIDTH", "HEIGHT"), type = int, help = "resolution of the internal video buffer (defaults to source resolution)")
+    arg_parser.add_argument("-R", "--resolution-scale", type = float, default = 50, help = "resolution of the internal video buffer compared to source resolution in percent (default is 50%%, max 100\\%%)")
     arg_parser.add_argument("-o", "--out", help = "output file name (defaults to 'YYYY-MM-DD_HH-MM-SS.mp4')")
     arg_parser.add_argument("--output-unscaled", action = "store_true", help = "output video at internal buffer resolution instead of source resolution")
     arg_parser.add_argument("-f", "--fps", type = int, help = "framerate of output video (default is 60, only applies to image sources)")
@@ -179,6 +174,8 @@ if __name__ == "__main__":
     # validate command line arguments
     if args.rate is not None and args.rate <= 0:
         raise ValueError("'rate' argument must be positive")
+    if args.resolution_scale is not None and (args.resolution_scale <= 0 or args.resolution_scale > 100):
+        raise ValueError("'resolution-scale' argument must be positive")
     if args.fps is not None and args.fps < 1:
         raise ValueError("'fps' argument must be positive")
     if args.duration is not None and args.duration <= 0:
